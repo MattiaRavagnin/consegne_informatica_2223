be5172498986898dfab00dfa0d4a720a99ecdc40
rava <mattia.ravagnin@itiszuccante.edu.it>
2022-11-14 19:13:36 +0100

    dev - client
    create vue project for client, add tailwind

---
+++ app-maps/maps-app-vue/maps-app-vue-client/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <h1 class="text-xl text-red-500">
+      Title
+    </h1>  
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  components: {
+    
+  }
+}
+</script>
