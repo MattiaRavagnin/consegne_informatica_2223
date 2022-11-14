f7f94fd9352e99bf043859155f7874daaad51be4
rava <mattia.ravagnin@itiszuccante.edu.it>
2022-11-14 21:56:52 +0100

    dev -client
    add leaflet, create default map/location

--- app-maps/maps-app-vue/maps-app-vue-client/src/views/Home.vue
+++ app-maps/maps-app-vue/maps-app-vue-client/src/views/Home.vue
@@ -1,17 +1,33 @@
 <template>
-  <div class="home">
-    <h1 class="text-xl text-red-500">
-      Title
-    </h1>  
+  <div class="h-screen relative">
+    <div id="map" class="h-full z-[1]"></div>
   </div>
 </template>
 
 <script>
+import leaflet from "leaflet";
+import { onMounted } from "vue";
 
 export default {
-  name: 'Home',
-  components: {
-    
-  }
-}
+  TOKEN:
+    "pk.eyJ1IjoibWF0dGlhcmF2YWduaW45OSIsImEiOiJjbGFoOGU2Z2EwNjE3M3VueWlsbXlwZGZ0In0.uVxGywDyvoeSZ0z9g96AqA",
+  name: "Home",
+  components: {},
+  setup() {
+    let map;
+    onMounted(() => {
+      //init map
+      map = leaflet
+        .map("map")
+        .setView([45.49597872984954, 12.257380498490015], 18); //TODO: insert default location (long,lat,zoom)
+
+      //add tile layer
+      leaflet
+        .tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+          maxZoom: 19,
+        })
+        .addTo(map);
+    });
+  },
+};
 </script>
