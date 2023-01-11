d0958bbe077ec41e4ebd3cd1f1b926a050927973
rava <mattia.ravagnin@itiszuccante.edu.it>
2023-01-11 17:56:47 +0100

    dev - client
    add router system with button for go to map

--- app-maps/maps-app-vue/maps-app-vue-client/src/router/index.js
+++ app-maps/maps-app-vue/maps-app-vue-client/src/router/index.js
@@ -1,17 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
+import Enter from '../views/Enter.vue'
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
+    path: '/map',
+    name: 'Map',
     component: Home
   },
   {
     path: '/login',
     name: 'Login',
     component: Login
+  },
+  {
+    path: '/',
+    name: 'Enter',
+    component: Enter
   }
 
 ]
---
+++ app-maps/maps-app-vue/maps-app-vue-client/src/views/Enter.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+
+    <div class="flex items-center justify-center h-screen">
+      <button @click="goToMap()">Click for Map</button>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import router from '../router/index'
+
+export default {
+  name: "Test",
+  created() {},
+  data() {
+    return {};
+  },
+  props: {},
+  methods: {
+    goToMap(){
+      router.push('/map')
+    }
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
